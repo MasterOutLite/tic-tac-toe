cdd3dccd5656322ce8ecb13415fea562aadeafb0
Legenda <88711334+MasterOutLite@users.noreply.github.com>
2022-12-21 23:52:09 +0200

    2

--- script.js
+++ script.js
@@ -2,6 +2,16 @@
 
 let isStepCross = false;
 let score = 9;
+const pointWin = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
 
 const whoseElement = document.querySelector("#whose");
 const scoreElement = document.querySelector("#score");
@@ -29,8 +39,31 @@ fields.forEach((element) => {
     score--;
     isStepCross = isStepCross ? false : true;
     printSData();
+    checkWiner();
+
     element.setAttribute("data-is-empty", "false");
   });
 });
 
 printSData();
+
+function checkWiner() {
+  pointWin.forEach((element) => {
+    let numberMatches = 1;
+    const typeField = fields[element[0]].textContent;
+
+    if (typeField === "") {
+      return;
+    }
+
+    for (let i = 1; i < element.length; i++) {
+      if (fields[element[i]].textContent === typeField) {
+        numberMatches++;
+        if (numberMatches >= 3) {
+          whoseElement.textContent = "Переможцем є: " + typeField;
+          console.log("Переможцем є: " + typeField);
+        }
+      }
+    }
+  });
+}
